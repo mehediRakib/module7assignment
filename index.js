25eb29e0b65bdbfd932f9bb7306eaac99f06f42c
Mehedi Hasan <couict6@gmail.com>
2023-09-12 22:15:17 +0600

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app=require('./app');
 require('dotenv').config();
-const port=process.env.RUNNING_PORT||5500;
+const port=process.env.RUNNING_PORT||5500 ;
 
 app.listen(port,()=>{
     console.log("server create succesfull");
